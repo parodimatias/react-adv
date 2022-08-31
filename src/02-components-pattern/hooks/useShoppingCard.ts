c06012bf7898d65c98a00848687782be92484765
Matias Parodi <matias.parodi@outlook.com>
2022-08-31 17:34:30 -0300

    Control props alt

--- src/02-components-pattern/hooks/useShoppingCard.ts
+++ src/02-components-pattern/hooks/useShoppingCard.ts
@@ -17,12 +17,11 @@ export const useShoppingCard = () => {
         ...product,
         count: 0,
       };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return { ...oldShoppingCart, [product.id]: productInCart };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        return rest;
       }
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
+      return { ...oldShoppingCart, [product.id]: productInCart };
     });
   };
   return { shoppingCart, onProductCountChange };
